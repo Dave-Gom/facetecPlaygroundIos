f8e4d254d21faece27337b3cfdeb0df8b5e41677
Dave Gomez <davegomez426@gmail.com>
2026-02-27 09:54:16 -0300

    Initial Commit

---
+++ facetecPlayground/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  facetecPlayground
+//
+//  Created by Dave Gomez on 2026-02-27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
